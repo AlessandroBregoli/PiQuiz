27d0c5ae5491e1a28abea54cec9516734c842057
root <root@Alessandro>
2014-05-23 13:43:17 +0200

    modificato file tv.js in modo che possa elimiare gli utenti inattivi

--- static/tv.js
+++ static/tv.js
@@ -2,4 +2,7 @@ var mainLoop = setInterval(mainLoopInterval , function(){
 		getStep(entryFile + "TVEventi", function(risposta){
 			
 		});
+		getStep(entryFile + "pulisciAccessi",function(risposta){
+			//probabilmente questa funzione non serve
+		});
 	}
